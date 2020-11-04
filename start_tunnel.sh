2f4d2f3a892d2a9a8b0a6ab4833c9447970815aa
scidev <scidev@scidev>
2020-11-04 10:43:29 +0100

    Kleine wijziging

--- start_tunnel.sh
+++ start_tunnel.sh
@@ -2,7 +2,7 @@
 if [ "$0" == "/media/Data3/rstudio/tunnel/rstudio/start_tunnel.sh" ]; then
 	sleep 90
 fi
-#nohup /media/Data3/rstudio/bin/ngrok http 8787 -region eu -config=/home/scidev/.ngrok2/ngrok_rstudio-server.yml >& /dev/null &
+nohup /media/Data3/rstudio/bin/ngrok http 8787 -region eu -config=/home/scidev/.ngrok2/ngrok_rstudio-server.yml >& /dev/null &
 sleep 60
 /media/Data3/rstudio/tunnel/rstudio/get_tunnel.py
 cd /media/Data3/rstudio/tunnel/rstudio
